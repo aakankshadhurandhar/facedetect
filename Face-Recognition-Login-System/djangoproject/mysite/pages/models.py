661bf95d200322e676158c49a673954e1f0f6b20
ayu_22 <ayu123shi@gmail.com>
2019-07-22 18:22:08 +0530

    added django-vote

--- Face-Recognition-Login-System/djangoproject/mysite/pages/models.py
+++ Face-Recognition-Login-System/djangoproject/mysite/pages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from vote.models import VoteModel
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -21,3 +22,12 @@ def create_profile(sender,**kwargs):
         user_profile=UserProfile.objects.get_or_create(user=kwargs['instance'])
 
 post_save.connect(create_profile,sender=User)
+
+
+
+class ArticleReview(VoteModel, models.Model):
+    ...
+
+
+
+
